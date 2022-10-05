7bb93fe5871508c425d42cef5229b4eac9043d23
moin khan <khansmoon999@gmail.com>
2022-10-05 16:05:04 +0400

    height

--- lib/animation/animation_list.dart
+++ lib/animation/animation_list.dart
@@ -5,6 +5,8 @@ class AnimationList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 200,
+    );
   }
 }
